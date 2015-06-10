735f5e01a32097b625260f57259ad33f194911fb
Daniel Riera <danielriera.p@gmail.com>
2015-06-10 17:13:59 +0200

    Adds header content-type

---
+++ Wordpress Plugin/phonegap_connect/datos.php
@@ -0,0 +1,106 @@
+<?php
+header('Content-Type: application/json');
+/*
+Plugin Name: Phonegap Connect
+Description: Conexión a Wordpress desde Phonegap
+Author: Daniel Riera
+Version: 1.0.0
+*/
+function guardarOpciones() {
+	
+}
+function pho_panel_opciones(){
+$opciones = get_option('pho_opciones',$valores_default);
+register_setting( 'pho_opciones_connect', 'pho_comentarios', 'pho_validar' );
+
+
+
+add_menu_page(
+'Opciones - Phonegap Connect',
+'Phonegap', //Nombre a mostrar panel opciones
+
+'administrator',//nivel de usuario que puede ver el panel
+
+'phonegap-connect', //identificador de mi panel dentro de wordpress
+
+'pho_opciones_panel', //función a la que llamar y que mostrará las opciones dentro del panel
+
+plugins_url( 'phonegap_connect/img/icon.png' )
+);
+
+}
+function pho_opciones_panel(){
+echo "
+<h1>Opciones Plugin Phonegap</h1>
+<p>Aquí podrás realizar las configuraciones para conectar tu aplicación PhoneGap con Wordpress.</p>";
+?>
+
+    <?php
+ if(isset($_POST['action']) && $_POST['action'] == "salvaropciones"){
+        update_option('pho_limite_caracteres',$_POST['caracteres_limite']);
+		update_option('pho_token',$_POST['token']);
+        update_option('pho_mostrar_comentarios',$_POST['comentarios']);
+        update_option('pho_mostrar_categorias_vacias',$_POST['cat_vacias']);
+        echo("<div class='updated message' style='padding: 10px'>Opciones guardadas.</div>");
+    }
+ 
+    ?>
+ <style>
+ table tr td {
+	 padding:10px; 
+ }
+ .nota {
+	 color:#666;
+	 font-size:10px;	 
+	}
+ </style>
+    <form method='post'>
+        <input type='hidden' name='action' value='salvaropciones'> 
+        <table>
+        <tr>
+                <td>
+                    Token de acceso
+                </td>
+                <td>
+                    <input type='text' name='token' id='token' value='<?=get_option('pho_token')?>' required>
+                    <span class="nota">Token para acceder a los datos.</span>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Limite caracteres categoria
+                </td>
+                <td>
+                    <input type='text' name='caracteres_limite' id='caracteres_limite' value='<?=get_option('pho_limite_caracteres')?>'>
+                    <span class="nota">Limite de caracteres del contenido de artículos en categorias.</span>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                  Mostrar comentarios
+                </td>
+                <td>
+                    <input type='checkbox' name='comentarios' id='comentarios' value='1' <?php checked(1== get_option('pho_mostrar_comentarios'));?> />
+                    <span class="nota">Mostrar los comentarios de los post, esto afecta tanto al listado de post en categoria como en la presentación del artículo.</span>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                   Mostrar Categorias Vacias
+                </td>
+                <td>
+                    <input type='checkbox' name='cat_vacias' id='cat_vacias' value='1' <?php checked(1== get_option('pho_mostrar_categorias_vacias'));?> />
+                    <span class="nota">Si está activado se mostrarán las categorias vacias.</span>
+                </td>
+            </tr>
+            <tr>
+                <td colspan='2'>
+                    <input type='submit' value='Enviar'>
+                </td>
+            </tr>
+        </table>
+    </form>
+	<?php echo "";
+}
+
+add_action('admin_menu', 'pho_panel_opciones');
