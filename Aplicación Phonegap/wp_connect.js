b6358fc2f54d148b447c4cd7e566660c534cf0e3
Daniel Riera <danielriera.p@gmail.com>
2016-06-27 21:52:44 +0200

    Añade versión min - Bugs fixed

--- Aplicación Phonegap/wp_connect.js
+++ Aplicación Phonegap/wp_connect.js
@@ -1,106 +1,94 @@
 /*
 
 Autor: Daniel Riera
-Version 2.0.1
+Version 2.0.2
 (C) 2016
 
 */
 url = null;
 token = null;
 var WP = (function() {
-watchCallback ="";
 	function categorias(callback) {
-		watchCallback ="";
 		checkStatus();
-		 watchCallback = callback;
 		$.ajax({
 			url : url+"/wp-content/plugins/phonegap_connect/categorias.php",
 			dataType : "JSON",
 			method : "POST",
 			data : "to="+token,
 			success : function(result) {
-				watchCallback(result);
+				callback(result);
 			}, error : function(e) {
 				console.log("Error CATEGORIES" + JSON.stringify(e));
 			}
 		});
 	}
 	function categoriaPost(callback,id) {
-		watchCallback ="";
 		console.log("ID" +id);
 		checkStatus();
-		 watchCallback = callback;
 		$.ajax({
 			url : url+"/wp-content/plugins/phonegap_connect/cat.php",
 			dataType : "JSON",
 			method : "POST",
 			data : "to="+token+"&id="+id,
 			success : function(result) {
-				watchCallback(result);
+				console.log(JSON.stringify(result));
+				callback(result);
 			}, error : function(e) {
-				console.log("Error CATEGORY");
+				console.log("Error CATEGORY" + JSON.stringify(e));
 			}
 		});
 	}
 	function homePost(callback) {
-		watchCallback ="";
 		checkStatus();
-		 watchCallback = callback;
 		$.ajax({
 			url : url+"/wp-content/plugins/phonegap_connect/home.php",
 			dataType : "JSON",
 			method : "POST",
 			data : "to="+token,
 			success : function(result) {
-				watchCallback(result);
+				callback(result);
 			}, error : function(e) {
 				console.log("Error HOME");
 			}
 		});
 	}
 	function post(callback,id){
-		watchCallback ="";
 		checkStatus();
-		 watchCallback = callback;
 		$.ajax({
 			url : url+"/wp-content/plugins/phonegap_connect/post.php",
 			dataType : "JSON",
 			method : "POST",
 			data : "to="+token+"&id="+id,
 			success : function(result) {
-				watchCallback(result);
+				callback(result);
 			}, error : function(e) {
 				console.log("Error POST");
 			}
 		});
 	}
 	function comenatarios(callback,id) {
-		watchCallback ="";
 		checkStatus();
-		 watchCallback = callback;
 		$.ajax({
 			url : url+"/wp-content/plugins/phonegap_connect/comentarios.php",
 			dataType : "JSON",
 			method : "POST",
 			data : "to="+token+"&id="+id,
 			success : function(result) {
-				watchCallback(result);
+				callback(result);
 			}, error : function(e) {
 				console.log("Error COMMENTS");
 			}
 		});
 	}
 	function pagina(callback,id) {
-		watchCallback ="";
 		checkStatus();
-		 watchCallback = callback;
 		$.ajax({
 			url : url+"/wp-content/plugins/phonegap_connect/pagina.php",
 			dataType : "JSON",
 			method : "POST",
 			data : "to="+token+"&id="+id,
 			success : function(result) {
-				watchCallback(result);
+				callback(result);
 			}, error : function(e) {
 				console.log("Error HOME");
 			}
